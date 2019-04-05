85ea84da2d53fc90ff4dc02fcc809c61dd60efad
kdnakt <a.kid.1985@gmail.com>
2019-04-05 23:28:47 +0900

    1.2.2.1. Testcase: List

---
+++ 1/testcase_list.rs
@@ -0,0 +1,37 @@
+use std::fmt;
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
+struct List2(Vec<i32>);
+
+impl fmt::Display for List2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1,2,3,4,5]);
+    println!("{}", v);
+
+    let v2 = List2(vec![1,2,3,4,5]);
+    println!("{}", v2);
+}
