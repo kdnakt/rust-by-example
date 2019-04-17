07aba69b5c00766e59638f37f2c3771e128d2ecd
kdnakt <a.kid.1985@gmail.com>
2019-04-17 22:44:25 +0900

    4. Variable bindings

---
+++ 4/variable_bindings.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    let _unused_variavle = 3u32;
+    let _noisy_unused_variable = 2u32;// Prefix with an underscore to suppress the warning
+}
