355be9555b197932ca6fd6a645d40d393ea1b069
kdnakt <a.kid.1985@gmail.com>
2019-04-21 21:42:58 +0900

    8.2.1. Nesting and labels

---
+++ 8/nesting_and_labels.rs
@@ -0,0 +1,17 @@
+#![allow(unreachable_code)]
+
+fn main() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
