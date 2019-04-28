97b2818603dd5a4ca7f133ab9bc91a8282340697
kdnakt <a.kid.1985@gmail.com>
2019-04-28 10:03:40 +0900

    9.2.3. Type anonymity

---
+++ 9/type_anonymity.rs
@@ -0,0 +1,14 @@
+fn apply<F>(f: F) where
+    F: Fn() {
+    f();
+}
+
+fn main() {
+    let x = 7;
+
+    // capture `x` into an anonymous type and implement
+    // `Fn` for it. Store it in `print`.
+    let print = || println!("{}", x);
+
+    apply(print);
+}
