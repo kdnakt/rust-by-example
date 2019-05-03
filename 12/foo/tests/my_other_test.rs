69f7324145c2d0af565041c7b8432c863f31efe4
kdnakt <a.kid.1985@gmail.com>
2019-05-03 10:56:55 +0900

    12.3. Testing

---
+++ 12/foo/tests/my_other_test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_bar() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_baz() {
+        assert_eq!(1 - 1, 0);
+    }
+}
---
+++ 12/foo/tests/my_test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_foo() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_foo_bar() {
+        assert_eq!(1 + 1, 2);
+    }
+}
