5003e3c44fb283c0339bec9d2c3bf98e63f7e329
kdnakt <a.kid.1985@gmail.com>
2019-05-05 19:54:27 +0900

    add not cfg

--- 13/custom.rs
+++ 13/custom.rs
@@ -3,8 +3,14 @@ fn conditional_function() {
     println!("condition met!");
 }
 
+#[cfg(not(some_condition))]
+fn conditional_function() {
+    println!("condition *not* met");
+}
+
 fn main() {
     conditional_function();
     // without `--cfg some_condition`,
     // rustc throws error: cannot find function `conditional_function` in this scope
+    // if `cfg(not())` exists
 }
