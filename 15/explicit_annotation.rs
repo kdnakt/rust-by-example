c1e5f7bd89b94ac6ef5c1966fea88f17370b2d09
kdnakt <a.kid.1985@gmail.com>
2019-05-20 23:49:50 +0900

    15.4.1. Explicit annotation

---
+++ 15/explicit_annotation.rs
@@ -0,0 +1,14 @@
+fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("x is {} and y is {}", x, y);
+}
+
+fn failed_borrow<'a>() {
+    let _x = 12;
+    //let y: &'a i32 = &_x;// error: &_x does not live long enough
+}
+
+fn main() {
+    let (four, nine) = (4, 9);
+    print_refs(&four, &nine);
+    failed_borrow();
+} 
