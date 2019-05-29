e384009f5b611850835a5bc6c31e9d41fc87a084
kdnakt <a.kid.1985@gmail.com>
2019-05-29 22:58:30 +0900

    16.5. Clone

---
+++ 16/clone.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, Clone, Copy)]
+struct Nil;
+
+#[derive(Clone, Debug)]
+struct Pair(Box<i32>, Box<i32>);
+
+fn main() {
+    let nil = Nil;
+    let copied_nil = nil;
+
+    println!("original: {:?}", nil);
+    println!("copy: {:?}", copied_nil);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+    println!("original: {:?}", pair);
+
+    let moved_pair = pair;
+    println!("copy: {:?}", moved_pair);
+
+    // error: use of moved value
+    //println!("original: {:?}", pair);
+
+    let cloned_pair = moved_pair.clone();
+    drop(moved_pair);
+
+    // error: use of moved value
+    //println!("copy: {:?}", moved_pair);
+
+    println!("clone: {:?}", cloned_pair);
+}
