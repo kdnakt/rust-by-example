7a3b6cb5a64f14c36ad9072ff08ff2772b6e3222
kdnakt <a.kid.1985@gmail.com>
2019-06-04 07:57:38 +0900

    17.4. Variadics

---
+++ 17/variadic_interface.rs
@@ -0,0 +1,20 @@
+macro_rules! calculate {
+    (eval $e:expr) => {{
+        {
+            let val : usize = $e;
+            println!("{} = {}", stringify!{$e}, val);
+        }
+    }};
+    (eval $e:expr, $(eval $es:expr),+) => {{
+        calculate! { eval $e }
+        calculate! { $(eval $es),+ }
+    }};
+}
+
+fn main() {
+    calculate! {
+        eval 1 + 2,
+        eval 3 + 4,
+        eval (2 * 3) + 1
+    }
+}
