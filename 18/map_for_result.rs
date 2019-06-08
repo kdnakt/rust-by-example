d15c424b51b02690d23a107919fd430878c08c10
kdnakt <a.kid.1985@gmail.com>
2019-06-09 08:44:58 +0900

    18.3.1. map for Result

---
+++ 18/map_for_result.rs
@@ -0,0 +1,39 @@
+use std::num::ParseIntError;
+
+fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    match first_number_str.parse::<i32>() {
+        Ok(first_number) => {
+            match second_number_str.parse::<i32>() {
+                Ok(second_number) => {
+                    Ok(first_number * second_number)
+                },
+                Err(e) => Err(e),
+            }
+        },
+        Err(e) => Err(e),
+    }
+}
+
+fn multiply_v2(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str.parse::<i32>().map(|second_number| first_number * second_number)
+    })
+}
+
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    let twenty = multiply("10", "2");
+    print(twenty);
+
+    let tt = multiply("t", "2");
+    print(tt);
+
+    let thirty = multiply_v2("5", "6");
+    print(thirty);
+}
