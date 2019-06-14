d5b29dfb75df531f0b4c7737fdb6dc0901ecfd39
kdnakt <a.kid.1985@gmail.com>
2019-06-14 23:11:02 +0900

    use map_or

--- 18/pulling_results_out_of_options.rs
+++ 18/pulling_results_out_of_options.rs
@@ -1,9 +1,13 @@
 use std::num::ParseIntError;
 
-fn double_first(vec: Vec<&str>) -> Option<Result<i32, ParseIntError>> {
-    vec.first().map(|first| {
+fn double_first(vec: Vec<&str>) -> Result<Option<i32>, ParseIntError> {
+    let opt= vec.first().map(|first| {
         first.parse::<i32>().map(|n| 2 * n)
-    })
+    });
+
+    let opt = opt.map_or(Ok(None), |r| r.map(Some))?;
+
+    Ok(opt)
 }
 
 fn main() {
