7226cbdbb5629e7940d0e6848dfa00e8fbf32cbd
kdnakt <a.kid.1985@gmail.com>
2019-07-06 09:46:59 +0900

    using chunks ..

--- 20/map_reduce.rs
+++ 20/map_reduce.rs
@@ -10,53 +10,49 @@ fn main() {
 523807952578 88236525459303330302837
 58495327135744041 048897885734297812
 699202164389808735488 08413720956532
-162784246374525898603453748 28574668";
+162784246374525898603453748 285746681";
 
     println!("Number of threads: {}", NTHREADS);
 
-    let mut children = vec![];
+    //let mut children = vec![];
 
-    // Map phase
-    let chunked_data = data.split_whitespace();
-    let chunk_count = chunked_data.clone().count() as f32;
-    println!("chunked data count: {}", chunk_count);
-    let nchunks = (chunk_count / NTHREADS).ceil();
-    println!("chunk_count / NTHREADS: {}", nchunks);
-    let mut res = "".to_string();
-    for (_, seg) in chunked_data.clone().enumerate() {
-        res.push_str(seg);
-    }
-    let len = res.len() as f32;
-    println!("res length: {}", len);
-    let end = (len / NTHREADS).ceil() as usize;
-    let (first, last) = res.split_at(end);
-    println!("first,last: {},{}", first, last);
-
-    for (i, data_segment) in chunked_data.enumerate() {
-        println!("data segment {} is \"{}\"", i, data_segment);
-        // without move, error: closure may outlive the current function, but it borrows i,
-        // which is owned by the current function
-        children.push(thread::spawn(move || -> u32 {
-            let result = data_segment
-                    .chars()
-                    .map(|c| c.to_digit(10).expect("should be a digit"))
-                    .sum();
-            println!("processed segment {}, result={}", i, result);
-
-            result
-        }));
+    // // Map phase
+    let chars: Vec<char> = data.chars()
+            .filter(|c| c.is_numeric())
+            .collect();
+    let mut itr = chars.chunks(15);
+    println!("it: {:?}", itr.next().unwrap());
+    let mut nxt = itr.next();
+    while !nxt.is_none() {
+        println!("it: {:?}", nxt.unwrap());
+        nxt = itr.next();
     }
 
-    // Reduce phase
-    let mut intermediate_sums = vec![];
-    for child in children {
-        let intermediate_sum = child.join().unwrap();
-        intermediate_sums.push(intermediate_sum);
-    }
+    // for (i, data_segment) in chunked_data.enumerate() {
+    //     println!("data segment {} is \"{}\"", i, data_segment);
+    //     // without move, error: closure may outlive the current function, but it borrows i,
+    //     // which is owned by the current function
+    //     children.push(thread::spawn(move || -> u32 {
+    //         let result = data_segment
+    //                 .chars()
+    //                 .map(|c| c.to_digit(10).expect("should be a digit"))
+    //                 .sum();
+    //         println!("processed segment {}, result={}", i, result);
+
+    //         result
+    //     }));
+    // }
+
+    // // Reduce phase
+    // let mut intermediate_sums = vec![];
+    // for child in children {
+    //     let intermediate_sum = child.join().unwrap();
+    //     intermediate_sums.push(intermediate_sum);
+    // }
 
     //let final_result = intermediate_sums.iter().sum::<u32>();
     // without turbofish ::<>, explicitly specifying the type
-    let final_result: u32 = intermediate_sums.iter().sum();
+    // let final_result: u32 = intermediate_sums.iter().sum();
 
-    println!("Final sum result: {}", final_result);
+    // println!("Final sum result: {}", final_result);
 }
