2407a325c3b8bdde54cebc3b875a52f3be9c1e07
kdnakt <a.kid.1985@gmail.com>
2019-07-07 22:17:04 +0900

    compile error ... chars does not live long enough

--- 20/map_reduce.rs
+++ 20/map_reduce.rs
@@ -14,45 +14,38 @@ fn main() {
 
     println!("Number of threads: {}", NTHREADS);
 
-    //let mut children = vec![];
+    let mut children = vec![];
 
     // // Map phase
     let chars: Vec<char> = data.chars()
             .filter(|c| c.is_numeric())
             .collect();
-    let mut itr = chars.chunks(15);
-    println!("it: {:?}", itr.next().unwrap());
+    let chunk_size = (chars.len() as f32 / NTHREADS).ceil();
+    println!("chunk_size: {}", chunk_size);
+    let mut itr = chars.chunks(chunk_size as usize);
+    
     let mut nxt = itr.next();
     while !nxt.is_none() {
-        println!("it: {:?}", nxt.unwrap());
+        let unwrapped = nxt.unwrap();
+        println!("processing: {:?}", unwrapped);
+        children.push(thread::spawn(move || -> u32 {
+            let result = unwrapped.iter()
+                .map(|c| c.to_digit(10).expect("should be a digit"))
+                .sum();
+            println!("processed result={}", result);
+            result
+        }));
         nxt = itr.next();
     }
 
-    // for (i, data_segment) in chunked_data.enumerate() {
-    //     println!("data segment {} is \"{}\"", i, data_segment);
-    //     // without move, error: closure may outlive the current function, but it borrows i,
-    //     // which is owned by the current function
-    //     children.push(thread::spawn(move || -> u32 {
-    //         let result = data_segment
-    //                 .chars()
-    //                 .map(|c| c.to_digit(10).expect("should be a digit"))
-    //                 .sum();
-    //         println!("processed segment {}, result={}", i, result);
+    // Reduce phase
+    let mut intermediate_sums = vec![];
+    for child in children {
+        let intermediate_sum = child.join().unwrap();
+        intermediate_sums.push(intermediate_sum);
+    }
 
-    //         result
-    //     }));
-    // }
-
-    // // Reduce phase
-    // let mut intermediate_sums = vec![];
-    // for child in children {
-    //     let intermediate_sum = child.join().unwrap();
-    //     intermediate_sums.push(intermediate_sum);
-    // }
-
-    //let final_result = intermediate_sums.iter().sum::<u32>();
-    // without turbofish ::<>, explicitly specifying the type
-    // let final_result: u32 = intermediate_sums.iter().sum();
-
-    // println!("Final sum result: {}", final_result);
+    let final_result = intermediate_sums.iter().sum::<u32>();
+    
+    println!("Final sum result: {}", final_result);
 }
