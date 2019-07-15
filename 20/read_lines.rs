7e4d296445c25cf4ce64ab2365fc437584bf3182
kdnakt <a.kid.1985@gmail.com>
2019-07-16 07:36:49 +0900

    20.4.3. read lines

---
+++ 20/read_lines.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./hosts") {
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
