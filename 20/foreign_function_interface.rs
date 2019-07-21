feb3ac6a4f1864c9ed07f72fc0c621f5515c36df
kdnakt <a.kid.1985@gmail.com>
2019-07-21 23:38:22 +0900

    20.8. Foreign Function Interface

---
+++ 20/foreign_function_interface.rs
@@ -0,0 +1,39 @@
+use std::fmt;
+
+#[link(name = "m")]
+extern {
+    fn csqrtf(z: Complex) -> Complex;
+    fn ccosf(z: Complex) -> Complex;
+}
+
+fn cos(z: Complex) -> Complex {
+    unsafe { ccosf(z) }
+}
+
+fn main() {
+    // z = -1 + 0i
+    let z = Complex { re: -1., im: 0. };
+
+    let z_sqrt = unsafe { csqrtf(z) };
+
+    println!("the square root of {:?} is {:?}", z, z_sqrt);
+
+    println!("cos({:?}) = {:?}", z, cos(z));
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct Complex {
+    re: f32,
+    im: f32,
+}
+
+impl fmt::Debug for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.im < 0. {
+            write!(f, "{}-{}i", self.re, -self.im)
+        } else {
+            write!(f, "{}+{}i", self.re, self.im)
+        }
+    }
+}
