deee127b88ce5266b2ab343af49584b52dced2b1
kdnakt <a.kid.1985@gmail.com>
2019-07-23 07:57:32 +0900

    add try_main

--- 21/doc/src/lib.rs
+++ 21/doc/src/lib.rs
@@ -38,3 +38,23 @@ pub fn div(a: i32, b: i32) -> i32 {
 
     a / b
 }
+
+/// Using hidden `try_main` in doc tests.
+///
+/// ```
+/// # // hidden lines start with `#` symbol, but they're still compileable.
+/// # fn try_main() -> Result<(), String> { // line that wraps the body shown in doc
+/// let res = doc::try_div(10, 2)?;
+/// # Ok(()) // returning from try_main
+/// # }
+/// # fn main() { // starting main that'll unwrap()
+/// #     try_main().unwrap(); // calling try_main and unwrapping
+/// #                          // so that test will panic in case of error
+/// # }
+pub fn try_div(a: i32, b: i32) -> Result<i32, String> {
+    if b == 0 {
+        Err(String::from("Division-by-zero"))
+    } else {
+        Ok(a / b)
+    }
+}
