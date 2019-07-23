f9f933f0d26958339d1b6bb3222af207f5b265cd
kdnakt <a.kid.1985@gmail.com>
2019-07-24 07:59:27 +0900

    21.4. Development dependencies

---
+++ 21/dev_deps/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
